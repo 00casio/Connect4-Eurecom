2d2181d9a1ca5bd5ff8652cfd6d2887d392203cf
Henri Gasc <gasc@eurecom.fr>
2023-02-14 19:08:07 +0100

    use black and isort, remove useless content

--- interface.py
+++ interface.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-from time import sleep
 import sys
+from time import sleep
 
 import numpy as np
 import pygame as pg
@@ -11,7 +11,14 @@ from variables import *
 
 # Board
 board = np.array(
-    [[no_player] * 7, [no_player] * 7, [no_player] * 7, [no_player] * 7, [no_player] * 7, [no_player] * 7]
+    [
+        [no_player] * 7,
+        [no_player] * 7,
+        [no_player] * 7,
+        [no_player] * 7,
+        [no_player] * 7,
+        [no_player] * 7,
+    ]
 )
 
 # initializing pygame window
@@ -21,6 +28,7 @@ screen = pg.display.set_mode((width_screen, height_screen), 0, 32)
 pg.display.set_caption(screen_title)
 board_surface = pg.surface.Surface((width_board, height_board)).convert_alpha()
 
+
 def update_screen(rect=None, pause=0):
     screen.blit(board_surface, (padding, padding))
     if rect is not None:
@@ -29,10 +37,12 @@ def update_screen(rect=None, pause=0):
         pg.display.update()
     sleep(pause)
 
+
 def draw_circle(n, m, color):
-    x = n*size_cell + size_cell//2
-    y = m*size_cell + size_cell//2
+    x = n * size_cell + size_cell // 2
+    y = m * size_cell + size_cell // 2
     pg.draw.circle(board_surface, color, (x, y), radius_disk)
+
 
 def start_game():
     screen.fill(color_screen)
@@ -41,6 +51,7 @@ def start_game():
         for j in range(6):
             draw_circle(i, j, color_trans)
     update_screen()
+
 
 def inverse_player(playing):
     if playing == player_1:
@@ -51,23 +62,26 @@ def inverse_player(playing):
         color_playing = color_player_1
     return playing, color_playing
 
+
 def find_low_bound(i):
     col = board[:, i]
-    for j in range(len(col)-1, -1, -1):
+    for j in range(len(col) - 1, -1, -1):
         if col[j] == no_player:
             return j
     return -1
 
+
 def click(player, color, pos_click_x):
     global board
 
-    num_col = pos_click_x//size_cell
+    num_col = pos_click_x // size_cell
     i = find_low_bound(num_col)
     if i == -1:
         return player, color
     board[i, num_col] = player
     draw_circle(num_col, i, color)
     return inverse_player(playing)
+
 
 start_game()
 playing = player_1
@@ -82,7 +96,9 @@ while True:
         elif event.type == pg.MOUSEBUTTONDOWN:
             pos_click = pg.mouse.get_pos()
             if padding < pos_click[0] < width_board + padding:
-                playing, color_playing = click(playing, color_playing, pos_click[0] - padding)
+                playing, color_playing = click(
+                    playing, color_playing, pos_click[0] - padding
+                )
 
         mouse_x, mouse_y = pg.mouse.get_pos()
         if mouse_x < pos_min_x:
@@ -90,125 +106,6 @@ while True:
         elif mouse_x > pos_max_x:
             mouse_x = pos_max_x
         screen.fill(color_screen)
-        pg.draw.circle(screen, color_playing, (mouse_x, padding//2), radius_disk)
-    update_screen()
+        pg.draw.circle(screen, color_playing, (mouse_x, padding // 2), radius_disk)
+    update_screen((0, 0, padding, width_screen))
     CLOCK.tick(fps)
-
-"""
-def draw_status():
-    global draw, winner, player
-
-    if winner is None:
-        message = player.upper() + "'s Turn"
-    else:
-        message = winner.upper() + " won!"
-    if draw:
-        message = "Game Draw!"
-
-    font = pg.font.Font(None, 30)
-    text = font.render(message, 1, (255, 255, 255))
-
-    # copy the rendered message onto the board
-    screen.fill((0, 0, 0), (0, 400, 500, 100))
-    text_rect = text.get_rect(center=(W / 2, 500 - 50))
-    screen.blit(text, text_rect)
-    pg.display.update()
-
-
-def check_win():
-    global Board, winner, draw, player
-
-    # check for winning rows
-    for row in range(0, 6):
-        ligne = Board[row, :]
-        for col in range(0, 4):
-            if ligne[col] is not None:
-                if ligne[col] == ligne[col + 1] == ligne[col + 2] == ligne[col + 3]:
-                    winner = player
-                    pg.draw.line(
-                        screen,
-                        (0, 255, 0),
-                        (col * j_w + j_w / 2, row * j_h + j_h / 2),
-                        ((col + 3) * j_w + j_w / 2, row * j_h + j_h / 2),
-                        10,
-                    )
-                    break
-
-    # check for winning columns
-    for col in range(0, 7):
-        column = Board[:, col]
-        for row in range(0, 3):
-            if column[row] is not None:
-                if column[row] == column[row + 1] == column[row + 2] == column[row + 3]:
-                    winner = player
-                    pg.draw.line(
-                        screen,
-                        (0, 255, 0),
-                        (col * j_w + j_w / 2, row * j_h + j_h / 2),
-                        (col * j_w + j_w / 2, (row + 3) * j_h + j_h / 2),
-                        10,
-                    )
-                    break
-
-    # check for winning diagonals
-    for row in range(3, 6):
-        for col in range(0, 4):
-            if Board[row, col] is not None:
-                if (
-                    Board[row, col]
-                    == Board[row + 1, col]
-                    == Board[row + 2, col]
-                    == Board[row + 2, col]
-                ):
-                    pg.draw.line(screen, (0, 255, 0), (0, H), (W, 0), 20)
-                    break
-
-    pg.display.update()
-
-
-def drawXO(row, col):
-    global Board, player
-
-    posx = j_w * (row) + 5
-    posy = j_h * (col) + 5
-    Board[row][col] = player
-
-    if player == "red":
-        screen.blit(r_img, (posy, posx))
-        player = "blue"
-    else:
-        screen.blit(b_img, (posy, posx))
-        player = "red"
-    pg.display.update()
-
-    # print(posx, posy)
-    # print(Board)
-
-
-def userClick():
-    # get coordinates of mouse click
-    x, y = pg.mouse.get_pos()
-
-    # get column (0-6) and row (0-5) of mouse click
-    col = int(x // j_w)
-    row = int(y // j_h)
-    # print(row, col)
-
-    if Board[row][col] is None:
-        global player
-        # draw the jeton on screen
-        drawXO(row, col)
-        check_win()
-
-
-def reset_game():
-    global Board, winner, player, draw
-    time.sleep(3)
-    player = "red"
-    draw = False
-    winner = None
-    Board = np.array(
-        [[None] * 7, [None] * 7, [None] * 7, [None] * 7, [None] * 7, [None] * 7]
-    )
-    game_opening()
-"""
--- variables.py
+++ variables.py
@@ -21,14 +21,14 @@ color_screen = white
 # Size
 size_cell = 100
 padding = size_cell
-width_board = 7*size_cell
-height_board = 6*size_cell
+width_board = 7 * size_cell
+height_board = 6 * size_cell
 radius_disk = 40
-width_screen = 2*padding + width_board
-height_screen = 2*padding + height_board
+width_screen = 2 * padding + width_board
+height_screen = 2 * padding + height_board
 
 # Pygame
 fps = 30
 screen_title = "Connect 4"
-pos_min_x = padding + size_cell//2
-pos_max_x = padding + width_board - size_cell//2
+pos_min_x = padding + size_cell // 2
+pos_max_x = padding + width_board - size_cell // 2
