609c7ccb0a5973c5f8508eb22c777ac366acccf0
Henri Gasc <gasc@eurecom.fr>
2023-02-17 12:00:55 +0100

    Automate launch

--- gesture.py
+++ gesture.py
@@ -3,11 +3,7 @@ import mediapipe as mp
 import pyautogui
 import math
 from enum import IntEnum
-from ctypes import cast, POINTER
-from comtypes import CLSCTX_ALL
-from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 from google.protobuf.json_format import MessageToDict
-import screen_brightness_control as sbcontrol
 
 pyautogui.FAILSAFE = False
 mp_drawing = mp.solutions.drawing_utils
--- launch.sh
+++ launch.sh
@@ -4,4 +4,14 @@ test() {
     python3.11 -c "import $1"
     if [ $? != 0 ]; then
         python3.11 -m pip install $1
+    fi
 }
+
+to_install="pygame numpy mediapipe pyautogui"
+
+for module in ${to_install}; do
+    test ${module}
+done
+
+python3.11 main.py
+python3.11 gesture.py
