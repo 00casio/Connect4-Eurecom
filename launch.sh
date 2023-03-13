cd00f2e64e7585c845a17555e55235406e9a2460
Henri Gasc <gasc@eurecom.fr>
2023-03-13 14:19:56 +0100

    Update launch program

--- launch.sh
+++ launch.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 
-p3=$(python -c "import sys; print(sys.version_info[:2])")
-
-if [ "$p3" == "(3, 11)" ]; then
-  echo "Using python 3.11"
-elif [ "$p3" == "(3, 10)" ]; then
+com="python3.11"
+python3.11 -c ""
+if [ $? -ne 0 ]; then
+  python3.10 -c ""
+  if [ $? -eq 0 ]; then
   echo "Using python 3.10"
+  com="python3.10"
+  else
+    echo "Could not find correct python version"
+    exit 1
+  fi
 else
-  echo "Could not find correct python version"
-  exit 1
+  echo "Using python 3.11"
 fi
 
 test() {
-    python -c "import $1" 1>/dev/null 2>&1
+    $1 -c "import $2" 1>/dev/null 2>&1
     if [ $? != 0 ]; then
-        python -m pip install $1
+        $1 -m pip install $2
     fi
 }
 
 to_install="pygame numpy mediapipe pyautogui playsound pygobject"
 
 for module in ${to_install}; do
-    test ${module}
+    test ${com} ${module}
 done
 
-python main.py
+${com} main.py
