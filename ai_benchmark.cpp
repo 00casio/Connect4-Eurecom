4dd58dbf5a5564fbb4021e1689b03c98e3eda5b0
Henri Gasc <gasc@eurecom.fr>
2023-03-24 16:59:17 +0100

    benchmarking

--- ai_benchmark.cpp
+++ ai_benchmark.cpp
@@ -8,6 +8,7 @@ int main(int argc, char **argv) {
     int nbr_test = 1000;
     int nbr_depth = 11;
     FILE *fp;
+    FILE *fp_tmp;
     for (int depth = 1; depth < nbr_depth; depth++) {
         double t = 0;
         for (int i = 0; i < nbr_test; i++) {
@@ -17,14 +18,17 @@ int main(int argc, char **argv) {
             clock_t end = clock();
             t += (double) (end - start) / CLOCKS_PER_SEC;
             printf("\r%i", i);
+            if (depth > 7) {
+                fp_tmp = fopen("tmp.out", "w");
+                fprintf(fp_tmp, "%i", i);
+                fclose(fp_tmp);
+            }
         }
-        printf("\r%i\n", nbr_test);
+        printf("\r%i: ", nbr_test);
         fprintf(stdout, "%i is done, it took %f seconds\n", depth, t);
-        fp = fopen("result.out","a");
-        fprintf(fp, "%5i | %25s | %10i | %15f\n", depth, argv[1], nbr_test, t/nbr_test);
+        fp = fopen("result.csv","a");
+        fprintf(fp, "%5i, %25s, %10i, %15f, %10s\n", depth, argv[1], nbr_test, t/nbr_test, argv[2]);
         fclose(fp);
     }
-    fp = fopen("result.out", "a");
-    fprintf(fp, "----- | ------------------------- | ---------- | ---------------\n");
     return 0;
 }
