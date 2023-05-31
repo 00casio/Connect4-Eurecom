42ce6d2f344781c6ac531b2a734e01402ac0d618
Cindy Do <cindy.do@eurecom.fr>
2023-05-31 14:29:52 +0200

    Modifications made and timeout implementation

--- communication/client.py
+++ communication/client.py
@@ -1,29 +1,36 @@
-import sys
-
+import select
 import bluetooth
+import sys
+from time import time
+
+# Define the game board and other necessary variables
+game_board = [[' ' for _ in range(7)] for _ in range(6)]
+current_player = 'X'  # Player X starts
 
 def print_game_board(game_state):
     # Print the received game state
     game_board = [list(game_state[i:i+7]) for i in range(0, len(game_state), 7)]
     pass
 
+
 def send_request_response(code):
     # Send request or response code to the server
-    sock.send(code.encode())
+    server_sock.send(code.encode())
 
 def receive_request_response():
     # Receive request or response code from the server
-    data = sock.recv(3).decode()
+    data = server_sock.recv(3).decode()
     return data
 
 def send_player_move(column):
     # Send player's move (column number) to the server
-    sock.send(column.encode())
+    server_sock.send(column.encode())
 
 def receive_game_state():
     # Receive the updated game state from the server
-    game_state = sock.recv(42).decode()  # Assuming the game state is always 42 characters long
+    game_state = server_sock.recv(42).decode()  # Assuming the game state is always 42 characters long
     return game_state
+
 
 def check_win(row, column):
     pass
@@ -60,120 +67,194 @@ def main():
     print("Connecting to \"{}\" on {}".format(name, host))
 
     # Create the client socket
-    sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-    sock.connect((host, port))
+    client_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+    client_sock.connect((host, port))
     
     print("Connected to server at", server_address)
 
-    # Receive request from the server
-    request = receive_request_response()
+    # Send request to the server to choose the game mode 
+    request = input("Enter 100 for human mode or 101 for AI mode. Do you accept the game? (yes/no):")
+    send_request_response(request)
 
-    if request == "100":
-        print("Received request to play in human mode.")
-        response = input("Do you accept the game? (yes/no): ")
+    # Receive the response from the server
+    response = receive_request_response()
 
-        if response.lower() == "yes":
-            send_request_response("102")  # Accept the game
-            print("Game request accepted. Waiting for the game board...")
+    if response == "102":
+        print("Game request accepted. Starting the game...")
 
-            # Receive the initial game state from the server
-            game_state = receive_game_state()
-            print("Initial game board:")
-            print_game_board(game_state)
+        if request == "100":
+            print("Start of the human mode")
+            response = input("Do you want to play yourself or with your AI? (player/ai): ")
 
-            # Game loop
-            while True:
-                # Prompt the player for a move
-                column = input("Enter the column number (0-6) to place your token: ")
-                if column.isdigit() and 0 <= int(column) <= 6:
-                    send_player_move(column)
-                    game_state = receive_game_state()
-                    print("Updated game board:")
-                    print_game_board(game_state)
-                else:
-                    print("Invalid column number. Please try again.")
-
-                # Check for game end conditions (win or draw)
-                if check_win(row, column):
-                    print("Player", current_player, "wins!")
-                    break
-                elif check_draw():
-                    print("It's a draw!")
-                    break
-
-        else:
-            send_request_response("103")  # Refuse the game
-            print("Game request refused.")
-    
-    elif request == "101":
-        print("Received request to play in AI mode.")
-        response = input("Do you want to play yourself or the AI? (player/ai): ")
-
-        if response.lower() == "player":
-            send_request_response("102")  # Accept the game with player mode
-            print("Game request accepted. Waiting for the game board...")
-
-            # Receive the initial game state from the server
-            game_state = receive_game_state()
-            print("Initial game board:")
-            print_game_board(game_state)
-
-            # Game loop
-            while True:
-                # Prompt the player for a move
-                column = input("Enter the column number (0-6) to place your token: ")
-                if column.isdigit() and 0 <= int(column) <= 6:
-                    send_player_move(column)
-                    game_state = receive_game_state()
-                    print("Updated game board:")
-                    print_game_board(game_state)
-                else:
-                    print("Invalid column number. Please try again.")
-
-                # Check for game end conditions (win or draw)
-                if check_win(row, column):
-                    print("Player", current_player, "wins!")
-                    break
-                elif check_draw():
-                    print("It's a draw!")
-                    break
-
-
-        elif response.lower() == "ai":
-            send_request_response("102")  # Accept the game with AI mode
-            print("Game request accepted. Waiting for the game board...")
-
-            # Receive the initial game state from the server
-            game_state = receive_game_state()
-            print("Initial game board:")
-            print_game_board(game_state)
-
-            # Game loop 
-            while True:
-                # AI playing 
-                ai_move = generate_ai_move()
-                send_player_move(ai_move)
+            if response.lower() == "player":
+                # Receive the initial game state from the server
                 game_state = receive_game_state()
-                print("Updated game board:")
+                print("Initial game board:")
                 print_game_board(game_state)
 
-                # Check for game end conditions (win or draw)
-                if check_win(row, column):
-                    print("Player", current_player, "wins!")
-                    break
-                elif check_draw():
-                    print("It's a draw!")
-                    break
+                # Game loop
+                while True:
+                    # Prompt the player for a move
+                    column = input("Enter the column number (0-6) to place your token: ")
+                    if column.isdigit() and 0 <= int(column) <= 6:
+                        send_player_move(column)
+                        start_time = time()
+                        ready, _, _ = select.select([client_sock], [], [], 60)
+                        # Check if timeout occurred
+                        if not ready:
+                            print("Timeout occurred. Player took too long to make a move.")
+                            break
+                        game_state = receive_game_state()
+                        print("Updated game board:")
+                        # Stop the timer
+                        elapsed_time = time() - start_time
+                        print("Elapsed time for the move:", elapsed_time)
+                        # Update game board and display
+                        print_game_board(game_state)
+
+                        # Check for game end conditions (win or draw or quit)
+                        if check_win(row, column):
+                            print("Player", current_player, "wins!")
+                            break
+                        elif check_draw():
+                            print("It's a draw!")
+                            break
+                        elif column == "201":
+                            print("Player requested to quit the game.")
+                            break
+                    else:
+                        print("Invalid column number. Please try again.")
+           
+            elif response.lower() == "ai":
+                print("Waiting for the game board...")
+
+                # Receive the initial game state from the server 
+                game_state = receive_game_state()
+                print("Initial game board:")
+                print_game_board(game_state)
+
+                # Game loop
+                while True:
+                    # Prompt the player for a move
+                    column = input("Enter the column number (0-6) to place your token: ")
+                    if column.isdigit() and 0 <= int(column) <= 6:
+                        send_player_move(column)
+                        start_time = time()
+                        ready, _, _ = select.select([client_sock], [], [], 60)
+                        # Check if timeout occurred
+                        if not ready:
+                            print("Timeout occurred. Player took too long to make a move.")
+                            break
+                        game_state = receive_game_state()
+                        print("Updated game board:")
+                        # Stop the timer
+                        elapsed_time = time() - start_time
+                        print("Elapsed time for the move:", elapsed_time)
+                        # Update game board and display
+                        print_game_board(game_state)
+                        # Check for game end conditions (win or draw or quit)
+                        if check_win(row, column):
+                            print("Player", current_player, "wins!")
+                            break
+                        elif check_draw():
+                            print("It's a draw!")
+                            break
+                        elif column == "201":
+                            print("Player requested to quit the game.")
+                            break
+                    else:
+                        print("Invalid column number. Please try again.")    
+    
+        elif request == "101":
+            print("AI mode")
+            response = input("Do you want to play yourself or with your AI? (player/ai): ")
+
+            if response.lower() == "player":
+                print("Waiting for the game board...")
+
+                # Receive the initial game state from the server
+                game_state = receive_game_state()
+                print("Initial game board:")
+                print_game_board(game_state)
+
+                # Game loop
+                while True:
+                    # Prompt the player for a move
+                    column = input("Enter the column number (0-6) to place your token: ")
+                    if column.isdigit() and 0 <= int(column) <= 6:
+                        send_player_move(column)
+                        start_time = time()
+                        ready, _, _ = select.select([client_sock], [], [], 60)
+                        # Check if timeout occurred
+                        if not ready:
+                            print("Timeout occurred. Player took too long to make a move.")
+                            break
+                        game_state = receive_game_state()
+                        print("Updated game board:")
+                        # Stop the timer
+                        elapsed_time = time() - start_time
+                        print("Elapsed time for the move:", elapsed_time)
+                        # Update game board and display
+                        print_game_board(game_state)
+                        # Check for game end conditions (win or draw or quit)
+                        if check_win(row, column):
+                            print("Player", current_player, "wins!")
+                            break
+                        elif check_draw():
+                            print("It's a draw!")
+                            break
+                        elif column == "201":
+                            print("Player requested to quit the game.")
+                            break
+                    else:
+                        print("Invalid column number. Please try again.")
+
+
+            elif response.lower() == "ai":
+                print("Waiting for the game board...")
+
+                # Receive the initial game state from the server
+                game_state = receive_game_state()
+                print("Initial game board:")
+                print_game_board(game_state)
+
+                # Game loop 
+                while True:
+                    # AI playing 
+                    ai_move = generate_ai_move()
+                    send_player_move(ai_move)
+                    start_time = time()
+                    ready, _, _ = select.select([client_sock], [], [], 60)
+                    # Check if timeout occurred
+                    if not ready:
+                        print("Timeout occurred. Player took too long to make a move.")
+                        break
+                    game_state = receive_game_state()
+                    print("Updated game board:")
+                    print_game_board(game_state)
+
+                    # Check for game end conditions (win or draw)
+                    if check_win(row, column):
+                        print("Player", current_player, "wins!")
+                        break
+                    elif check_draw():
+                        print("It's a draw!")
+                        break
 
         else:
-            send_request_response("103")  # Refuse the game
-            print("Game request refused.")
+            print("Invalid request code.")
+
+    elif response == "103":
+        print("Game request refused by the server.")
+
     else:
-        print("Invalid request received.")
+        print("Invalid response received from the server.")
+
    
 
     # Clean up
-    sock.close()
+    client_sock.close()
+    print("Connection closed.")
 
 if __name__ == "__main__":
     main()
--- communication/server.py
+++ communication/server.py
@@ -1,4 +1,6 @@
 import os
+import bluetooth
+from time import time
 
 # Define the game board and other necessary variables
 game_board = [[' ' for _ in range(7)] for _ in range(6)]
@@ -14,6 +16,7 @@ def print_game_board():
     # Print the current game board
     pass
 
+
 def send_request_response(code):
     # Send request or response code to the client
     client_sock.send(code.encode())
@@ -22,34 +25,6 @@ def receive_request_response():
     # Receive request or response code from the client
     data = client_sock.recv(3).decode()
     return data
-
-def play_game():
-    global current_player
-
-    # Send initial game state to the client
-    send_game_state()
-
-    while True:
-        # Receive player move from the client
-        column = receive_player_move()
-        row = get_next_empty_row(column)
-
-        # Update the game board
-        game_board[row][column] = current_player
-
-        # Send updated game state to the client
-        send_game_state()
-
-        # Check for a win or draw
-        if check_win(row, column):
-            print("Player", current_player, "wins!")
-            break
-        elif check_draw():
-            print("It's a draw!")
-            break
-
-        # Switch to the other player
-        current_player = 'O' if current_player == 'X' else 'X'
 
 def send_game_state():
     # Convert game board to a string representation
@@ -80,6 +55,42 @@ def check_draw():
     # Check if the game is a draw (all cells are filled)
     pass
 
+def play_game():
+    global current_player
+
+    # Send initial game state to the client
+    send_game_state()
+
+    while True:
+        try:
+            # Receive player move from the client
+            column = receive_player_move()
+            row = get_next_empty_row(column)
+        except bluetooth.btcommon.BluetoothError:
+            print("Player took too long to make a move. Timeout occurred.")
+            break
+
+        # Update the game board
+        game_board[row][column] = current_player
+
+        # Send updated game state to the client
+        send_game_state()
+
+        # Check for a win or draw or quit 
+        if check_win(row, column):
+            print("Player", current_player, "wins!")
+            break
+        elif check_draw():
+            print("It's a draw!")
+            break
+        elif column == "201":
+            print("Player requested to quit the game.")
+            break
+
+        # Switch to the other player
+        current_player = 'O' if current_player == 'X' else 'X'
+
+
 def main():
 
     os.system("bluetoothctl discoverable on")
@@ -105,29 +116,35 @@ def main():
     # Game initialization
     initialize_game()
 
-    # Send request to the client to choose the game mode 
-    send_request_response("100") or send_request_response("101") 
-    response = receive_request_response()
+    # Set the timeout for receiving player's move to 60 seconds
+    client_sock.settimeout(60)
 
-    if response == "102":
-        print("Player accepted the game.")
+    # Receive the request from the client to choose the game mode  
+    request = receive_request_response()
+
+    if request == "100":
+        print("Start the game in human mode.")
+        # Send the response to the client
+        send_request_response(client_sock, "102")
         print_game_board()  # Launch of the board 
 
         # Play the game
         play_game()
+    elif request == "101":
+        print("Start the game in AI mode.")
+        # Send the response to the client
+        send_request_response(client_sock, "102")
+        # Start the game in AI mode
+        play_game()
     else:
-        print("Player refused the game.")
+        # Request was refused
+        print("Client refused the game request.")
+        send_request_response(client_sock, "103")
 
-     # Clean up
+    # Clean up
     client_sock.close()
     server_sock.close()
     print("All done.")
 
-
 if __name__ == "__main__":
     main()
-
-client_sock.close()
-server_sock.close()
-print("All done.")
-
