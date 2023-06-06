dc8d04e85b3fc55e91386bdf5e896bdc4a524c3c
Henri Gasc <gasc@eurecom.fr>
2023-06-06 18:06:00 +0200

    tests for interruptions 4/? client loop

--- core/screens.py
+++ core/screens.py
@@ -474,7 +474,8 @@ class OpponentSelectionScreen(Screen):
             boxes.append(tmp)
         self.all_boxes = []
         self.screen.fill(self.var.color_options_screen)
-        self.draw_agreement_box(f"We found {len(self.comm.connections)} possible opponents", position=0.20, hide=False)
+        if len(self.comm.connections) == 0:
+            self.draw_agreement_box(f"We found {len(self.comm.connections)} possible opponents", position=0.20, hide=False)
         self.center_all(boxes)
         return boxes
 
